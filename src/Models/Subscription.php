e8a563218e1cbd714bb899356bdf00c4924e9857
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2017-09-26 15:38:09 +0300

    initial model

---
+++ src/Models/Subscription.php
@@ -0,0 +1,24 @@
+<?php
+namespace Incontrl.Models;
+
+public class Subscription
+{
+    public function __construct() {
+        
+    }
+    public string Id;
+
+    public string Code;
+
+    public string Alias;
+
+    public string TimeZone;
+
+//    public SubscriptionStatus Status;
+
+//    public Organisation Company;
+
+//    public Contact Contact;
+
+    public string Notes;
+}
